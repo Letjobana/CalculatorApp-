a942e29196df0b50fafcaa26c1b896b9e1c07146
Suzan Masite <SuzanM@blts.co.za>
2023-05-28 22:54:49 +0200

    Added Controller classes for handling user requests

---
+++ CalculatorApp/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using CalculatorApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private CalculatorRepository calculator;
+
+        public CalculatorController()
+        {
+            calculator = new CalculatorRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Calculate(string expression)
+        {
+            try
+            {
+                double result = calculator.EvaluateExpression(expression);
+                return Json(new { success = true, result = result });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
+        }
+    }
+}
